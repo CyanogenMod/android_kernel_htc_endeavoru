9ea8eb1b24f245515fd96588aa16dc14e927c4f9
tbalden <illespal@gmail.com>
2013-03-04 21:56:38 +0100

    Revert "patch: update graphic drivers"
    This reverts commit 1e204991aac27367189201370bd548087ba8adc0.
    
    Outer cache part seemed
    to cause trouble on ICS blobs and OneX+ too.
    Better without it for now.

--- drivers/video/tegra/nvmap/nvmap.c
+++ drivers/video/tegra/nvmap/nvmap.c
@@ -53,11 +53,9 @@ static void map_iovmm_area(struct nvmap_handle *h)
 	for (va = h->pgalloc.area->iovm_start, i = 0;
 	     va < (h->pgalloc.area->iovm_start + h->size);
 	     i++, va += PAGE_SIZE) {
-		unsigned long pfn;
-
-		pfn = page_to_pfn(h->pgalloc.pages[i]);
-		BUG_ON(!pfn_valid(pfn));
-		tegra_iovmm_vm_insert_pfn(h->pgalloc.area, va, pfn);
+		BUG_ON(!pfn_valid(page_to_pfn(h->pgalloc.pages[i])));
+		tegra_iovmm_vm_insert_pfn(h->pgalloc.area, va,
+					  page_to_pfn(h->pgalloc.pages[i]));
 	}
 	h->pgalloc.dirty = false;
 }
