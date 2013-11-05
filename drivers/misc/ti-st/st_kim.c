6dfa42f4a19ee40d2379ad7a5d2c479bef7e5027
Thomas Wendt <thoemy@gmx.net>
2013-11-05 01:12:42 +0100

    misc: ti-st: Re-add QOS tuning support
    Change-Id: Ib3721ec54130aaf5ba608281e60aee5df56fbd6b

--- drivers/misc/ti-st/st_kim.c
+++ drivers/misc/ti-st/st_kim.c
@@ -39,6 +39,11 @@
 #include <linux/of.h>
 #include <linux/of_device.h>
 
+#define PM_QOS_TUNING_SUPPORTED
+#ifdef PM_QOS_TUNING_SUPPORTED
+#include <linux/pm_qos_params.h>
+#endif
+
 #define MAX_ST_DEVICES	3	/* Imagine 1 on each UART for now */
 static struct platform_device *st_kim_devices[MAX_ST_DEVICES];
 
@@ -679,6 +684,76 @@ static struct attribute_group uim_attr_grp = {
 	.attrs = uim_attrs,
 };
 
+#ifdef PM_QOS_TUNING_SUPPORTED
+static struct pm_qos_request_list st_pm_qos_latency_request;
+static struct pm_qos_request_list st_pm_qos_cpu_min_request;
+static struct pm_qos_request_list st_pm_qos_cpu_max_request;
+
+static unsigned char st_performance_lock_state;
+
+#define TI_ST_CREATE_DEVICE_ATTR(_name)         \
+        struct device_attribute dev_attr_##_name = {    \
+                .attr = {                               \
+                        .name = __stringify(_name),     \
+                        .mode = 0666 },                 \
+                .show = NULL,                           \
+                .store = NULL,                          \
+        }
+
+#define TI_ST_SET_DEVICE_ATTR(_name, _mode, _show, _store)      \
+        do {                                                    \
+                dev_attr_##_name.attr.mode = 0666;              \
+                dev_attr_##_name.show = _show;                  \
+                dev_attr_##_name.store = _store;                \
+        } while(0)
+
+static TI_ST_CREATE_DEVICE_ATTR(st_performance_lock);
+
+static struct attribute *st_performance_lock_attributes[] = {
+        &dev_attr_st_performance_lock.attr,
+        NULL 
+};
+
+static struct attribute_group st_performance_lock_attribute_group = {
+        .attrs = st_performance_lock_attributes
+};
+
+static ssize_t show_st_performance_lock(struct device *dev,
+            struct device_attribute *attr, char *buf)
+{
+        return sprintf(buf, "%d\n", st_performance_lock_state);
+}
+
+static ssize_t store_st_performance_lock(struct device *dev,
+            struct device_attribute *attr, const char *buf, size_t count)
+{
+        char in_char[] = "0";
+
+        sscanf(buf, "%1s", in_char);
+
+        if (strcmp(in_char, "0") == 0) {
+                st_performance_lock_state = 0;
+        }
+        else if (strcmp(in_char, "1") == 0) {
+                st_performance_lock_state = 1;
+        }
+        if (1 == st_performance_lock_state) {
+		//pm_qos_update_request(&st_pm_qos_latency_request, 30);
+		pm_qos_update_request(&st_pm_qos_cpu_min_request, 1000000);
+		pm_qos_update_request(&st_pm_qos_cpu_max_request, 1400000);
+                pr_info("pm_qos_update_request - cpu_min_request; cpu_max_request\n");
+        }
+        else if (0 == st_performance_lock_state) {
+		//pm_qos_update_request(&st_pm_qos_latency_request, PM_QOS_CPU_DMA_LAT_DEFAULT_VALUE);
+                pm_qos_update_request(&st_pm_qos_cpu_min_request, PM_QOS_CPU_FREQ_MIN_DEFAULT_VALUE);
+                pm_qos_update_request(&st_pm_qos_cpu_max_request, PM_QOS_CPU_FREQ_MAX_DEFAULT_VALUE);
+                pr_info("pm_qos_update_request - PM_QOS_CPU_DMA_LAT_DEFAULT_VALUE\n");
+        }
+        return 1;
+}
+#endif
+
+
 /**
  * st_kim_ref - reference the core's data
  *	This references the per-ST platform device in the arch/xx/
@@ -767,6 +842,12 @@ static int kim_probe(struct platform_device *pdev)
 	struct ti_st_plat_data	*pdata;
 	int err;
 
+#ifdef PM_QOS_TUNING_SUPPORTED
+//        pm_qos_add_request(&st_pm_qos_latency_request, PM_QOS_CPU_DMA_LATENCY, (s32)PM_QOS_CPU_DMA_LAT_DEFAULT_VALUE);
+	pm_qos_add_request(&st_pm_qos_cpu_max_request, PM_QOS_CPU_FREQ_MAX, (s32)PM_QOS_CPU_FREQ_MAX_DEFAULT_VALUE);
+	pm_qos_add_request(&st_pm_qos_cpu_min_request, PM_QOS_CPU_FREQ_MIN, (s32)PM_QOS_CPU_FREQ_MIN_DEFAULT_VALUE);
+#endif
+
 	if (pdev->dev.of_node)
 		pdata = get_platform_data(&pdev->dev);
 	else
@@ -826,6 +907,19 @@ static int kim_probe(struct platform_device *pdev)
 		pr_err("failed to create sysfs entries");
 		goto err_sysfs_group;
 	}
+
+#ifdef PM_QOS_TUNING_SUPPORTED
+        int result;
+        result = sysfs_create_group(&pdev->dev.kobj,
+                        &st_performance_lock_attribute_group);
+        if (result)
+                printk(KERN_ERR "%s reg attr fail!!",
+                                __func__);
+
+        TI_ST_SET_DEVICE_ATTR(st_performance_lock,
+                        0644, show_st_performance_lock,
+                        store_st_performance_lock);
+#endif
 
 	/* copying platform data */
 	strncpy(kim_gdata->dev_name, pdata->dev_name, UART_DEV_NAME_LEN);
